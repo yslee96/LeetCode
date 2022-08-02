ca7da96574eaf52bb33e1adb17f6e64a8062ccc7
yslee96 <77106988+yslee96@users.noreply.github.com>
2022-08-02 22:14:58 +0900

    Update 378-kth-smallest-element-in-a-sorted-matrix.py

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        #Approach1 : Binary_Search
         l = matrix[0][0]
         r = matrix[-1][-1]
         
@@ -13,4 +14,21 @@ class Solution:
             else:
                 r = m
         return l
+        
+        #Approach2 : simple sort
+        num_dict = {}
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] in num_dict:
+                    num_dict[matrix[i][j]] +=1
+                else:
+                    num_dict[matrix[i][j]] = 1
+        sorted_dict = sorted(num_dict.items())
+        cur_cnt = 0
+        for key, cnt in sorted_dict:
+            cur_cnt += cnt
+            if cur_cnt >=k:
+                answer = key
+                break
+                
         return answer
