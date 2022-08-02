f67b50dea9004f0b1dad28ac9f0d490bbbc24e59
yslee96 <77106988+yslee96@users.noreply.github.com>
2022-08-02 22:12:37 +0900

    Time: 330 ms (41.20%), Space: 18.8 MB (38.85%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,18 +1,16 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        num_dict = {}
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] in num_dict:
-                    num_dict[matrix[i][j]] +=1
-                else:
-                    num_dict[matrix[i][j]] = 1
-        sorted_dict = sorted(num_dict.items())
-        cur_cnt = 0
-        for key, cnt in sorted_dict:
-            cur_cnt += cnt
-            if cur_cnt >=k:
-                answer = key
-                break
-                
+        l = matrix[0][0]
+        r = matrix[-1][-1]
+        
+        while l < r:
+            m = l + (r - l) // 2
+            # print(m)
+            # calculate how many numbers are on the left of middle number
+            if sum(bisect.bisect_right(row, m) for row in matrix) < k:   
+
+                l = m + 1
+            else:
+                r = m
+        return l
         return answer
