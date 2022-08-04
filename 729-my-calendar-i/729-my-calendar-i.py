76f779bee8ef14fc88f8c00178380c1b3cf54dbf
yslee96 <77106988+yslee96@users.noreply.github.com>
2022-08-04 14:10:40 +0900

    Time: 450 ms (52.14%), Space: 15.3 MB (10.23%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,19 +1,31 @@
+from sortedcontainers import SortedList
 class MyCalendar:
 
     def __init__(self):
-        self.calendar = []
+        self.s = SortedList()
 
-    def book(self, start: int, end: int) -> bool:
-        if not self.calendar:
-            self.calendar.append([start,end])
-        else:
-            for i in range(len(self.calendar)):
-                if start<self.calendar[i][1] and end>=self.calendar[i][1]:
-                    return False
-                elif start>=self.calendar[i][0] and end<=self.calendar[i][1]:
-                    return False
-                elif start<self.calendar[i][0] and end>self.calendar[i][0]:
-                    return False
-            self.calendar.append([start,end])
-        return True       
+    def book(self, start, end):
+        if not self.s:
+            self.s.add([start,end])
+            return True
 
+        # Get the index of the new elment if its inserted into the sorted array
+        indx = self.s.bisect_left([start,end])
+
+        # If its the first element in the sorted list
+        if not indx:
+            if end<=self.s[indx][0]:
+                self.s.add([start,end])
+                return True
+
+        # If its the last element in the sorted list
+        elif indx == len(self.s):
+            if start >= self.s[indx-1][1]:
+                self.s.add([start,end])
+                return True
+
+        # If its the somewhere in the middle
+        elif self.s[indx-1][1]<=start and end<=self.s[indx][0]:
+            self.s.add([start,end])
+            return True
+        return False
