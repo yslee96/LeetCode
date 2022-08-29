30b009afcdcd9358cac2d735fb81f791644e69e4
yslee96 <77106988+yslee96@users.noreply.github.com>
2022-08-29 21:21:35 +0900

    Time: 161 ms (40.00%), Space: 14.2 MB (80.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -6,7 +6,6 @@ class Solution:
         acc_sums[0] = nums[0]
         for i in range(1,len(nums)):
             acc_sums[i] = acc_sums[i-1]+nums[i]
-        print(acc_sums)
         for query in queries:
             answers.append(bisect_right(acc_sums, query))
         return answers
