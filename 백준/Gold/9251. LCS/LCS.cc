a46d8f6b4192d29d6216f7b769edb59c61eb2963
yslee96 <77106988+yslee96@users.noreply.github.com>
2023-07-12 11:23:59 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+int dp[1001][1001];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str1, str2;
+    cin >> str1 >> str2;
+    for(int i=1; i<=str1.size(); i++){
+        for(int j=1; j<=str2.size(); j++){
+            if(str1[i-1] == str2[j-1])
+               dp[i][j] = dp[i-1][j-1]+1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    cout << dp[str1.size()][str2.size()] << "\n";
+    return 0;
+}   
