708a95af4ce6d159efbf4973dd9e6d23123f3371
Yunsu Lee <dbstn322@gmail.com>
2023-09-05 23:31:13 +0900

    Time: 367 ms (30.89%), Space: 25.2 MB (46.28%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        dp = defaultdict(lambda:-1)
+        def frog_jump(idx, k):
+            if idx >= len(stones):
+                return False
+            if idx == len(stones)-1:
+                return True    
+            if dp[(idx,k)]!=-1:
+                return dp[(idx,k)]
+            
+            for i in [-1, 0, 1]:
+                if idx ==0 and i!=0: continue
+                if k+i <=0: continue
+                next_pos = stones[idx] + k+i
+                next_idx = -1
+                for j in range(idx+1, len(stones)):
+                    if stones[j] > next_pos: break
+                    if stones[j] == next_pos:
+                        next_idx = j
+                if next_idx!=-1:
+                    print(stones[next_idx], k+i)
+                    if frog_jump(next_idx, k+i):
+                        dp[(idx,k)] = 1
+                        return dp[(idx,k)]
+
+            dp[(idx,k)] = 0
+            return dp[(idx,k)]
+        return frog_jump(0, 1)
+        
+        
