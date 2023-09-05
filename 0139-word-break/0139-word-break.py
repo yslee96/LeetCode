db1776ae4c74a93bd37dec0230120e3d7eab3f84
Yunsu Lee <dbstn322@gmail.com>
2023-09-05 16:32:06 +0900

    Time: 45 ms (60.14%), Space: 16.4 MB (58.33%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,14 +3,12 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [-1 for _ in range(len(s))]
         def check_if_break_possible(idx):
-            #print("call: ", idx)
             if idx >= len(s):
                 return 1
             if dp[idx]!=-1:
                 return dp[idx] == 1
             
             string = ''
-            result = False
             for i in range(idx, len(s)):
                 string += s[i]
                 if string in wordDict:
@@ -20,7 +18,6 @@ class Solution:
             dp[idx]=0
             return dp[idx]
 
-        ret = check_if_break_possible(0) == 1
-        #print(dp)
-        return ret
+        return True if check_if_break_possible(0) else False
+
         
