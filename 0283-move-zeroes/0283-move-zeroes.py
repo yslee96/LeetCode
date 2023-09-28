19de83d8446b6a73cb7eea34ba865bb3f61c67db
Yunsu Lee <dbstn322@gmail.com>
2023-09-28 22:33:59 +0900

    Time: 145 ms (62.3%), Space: 17.8 MB (75.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        idx = 0
-        zero_cnt = 0
-        while idx < len(nums):
-            if nums[idx] == 0:
-                zero_cnt+=1
-                del nums[idx]
-            else:
-                idx+=1
-        for _ in range(zero_cnt):
-            nums.append(0)
+        left = 0
+        for right in range(len(nums)):
+            if nums[right]!=0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left+=1
             
         
