1d710be4081f45dfa1f40bad2374f1fd11688454
Yunsu Lee <dbstn322@gmail.com>
2024-03-08 14:27:08 +0900

    [Gold II] Title: 공항, Time: 284 ms, Memory: 114844 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -1,24 +1,24 @@
 import sys
-sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 num_gates = int(input())
 num_planes = int(input())
 gates_to_dock = [int(input()) for _ in range(num_planes)]
 reps = [i for i in range(num_gates+1)]
-
 def find_rep(num):
     if reps[num] == num:
         return num
     reps[num] = find_rep(reps[num])
     return reps[num]
 
-result = 0
-for i in range(num_planes):
-    # 게이트 대표 번호에 dock
-    cur_rep_gate = find_rep(gates_to_dock[i])
-    # 처음에는 자기 번호 == 대표번호
-    if not cur_rep_gate: break
-    result+=1
-    # 도킹한 게이트의 대표번호(자기번호 이하)를 1씩 감소시키고 그 번호의 대표번호가 새로운 대표가됨
-    reps[cur_rep_gate] = find_rep(cur_rep_gate-1)
-print(result)
+def union_set(num1, num2):
+    reps[find_rep(num2)] = find_rep(num1)
+
+plane_cnt=0
+for gate_num in gates_to_dock:
+    actual_gate = find_rep(gate_num)
+    if actual_gate == 0:
+        break
+    plane_cnt+=1
+    union_set(actual_gate-1, actual_gate)
+    
+print(plane_cnt)
