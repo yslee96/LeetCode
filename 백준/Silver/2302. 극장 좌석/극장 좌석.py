843e49d3a72f06a87a87d5b4fefb0660042e98b6
Yunsu Lee <dbstn322@gmail.com>
2024-08-10 20:14:15 +0900

    [Silver I] Title: 극장 좌석, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.py
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -4,8 +4,10 @@ num_seats = int(input())
 num_vips = int(input())
 is_vip = [False for _ in range(num_seats+1)]
 for _ in range(num_vips):
-    is_vip[int(input())] = True
+    vip_num = int(input())
+    is_vip[vip_num] = True
 dp = [0 for _ in range(num_seats+1)]
+
 if num_seats == 1:
     print(1)
 else:
@@ -18,10 +20,3 @@ else:
         else:
             dp[i] = dp[i-1] + dp[i-2]
     print(dp[num_seats])
-    
-"""
-D[i] : VIP석 없이 1~i을 배치할 경우의 수
-D[N]을 구하기 위해 N번 학생이 어디에 앉는지로 케이스를 나눠봅시다.
-Case 1 - N번 학생이 N번 자리에 앉는 경우 : 1~N-1번 학생들을 잘 앉히면 되므로 D[N-1]
-Case 2 - N번 학생이 N-1번 자리에 앉는 경우 : N-1번 학생은 반드시 N번 자리에 앉아야하고, 나머지 1~N-2번 학생들을 잘 앉히면 되므로 D[N-2]
-"""
